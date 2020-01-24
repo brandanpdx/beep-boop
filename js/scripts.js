40ddfc5153e61ca90a9d99bfffd946e9a8278cbd
Brandan Sayarath <bsayarath@gmail.com>
2020-01-24 12:36:23 -0800

    refactor backend logic

--- js/scripts.js
+++ js/scripts.js
@@ -8,28 +8,25 @@ $(document).ready(function(){
       alert("Please enter numbers only!")
     }
 
-    $("ul#result").append("<li>" + beepboop(userNumber) + "</li>");
+    $("#result").append("<li>" + beepboop(userNumber) + "</li>");
   })
 })
 
 // Back-end logic
 function beepboop(num) {
-  var array = []
-
-  for (var numbers = 0; numbers <= num; numbers ++) {
-    array.push(numbers);
+  var numbers = [];
+  for (var index = 0; index <= num; index ++) {
+    numbers.push(index);
   }
-
-  for (var index = 0; index >= array; index ++) {
-    if (index.toString().includes("3")) {
-      array[index] = "I'm sorry, Dave.  I'm afraid I can't do that."
-    } else if (index.toString().includes("2")) {
-      array[index] = "Boop" 
-    } else if (index.toString().includes("1")) {
-      array[index] = "Beep" 
-    } 
+  for (var i = 0; i <= num; i ++) {
+    if (i.toString().includes("3")) {
+      numbers[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
+    } else if (i.toString().includes("2")) {
+      numbers[i] = "Boop!";
+    } else if (i.toString().includes("1")) {
+      numbers[i] = "Beep!";
+    }
   }
-  return array;
+  return numbers;
 }
-
 
