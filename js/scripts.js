741bc19383d48bb87c6ec7f60fbd2fbe2087515d
Brandan Sayarath <bsayarath@gmail.com>
2020-01-24 10:53:25 -0800

    add initial back-end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,31 @@
+// Back-end logic
+function beepboop(num) {
+  var userNumber = parseInt($("input#numberinputfield").val());
+  var numArray = []
+
+  for (var number = 0; number <= userNumber; number ++) {
+
+    numArray.push(number);
+    console.log(numArray);
+  }
+}
+
+
+
+// Front-end logic
 $(document).ready(function(){
   $(".numberinputform").submit(function(event) {
     event.preventDefault();
-    var number = $("input#numberinputfield").val()
+    var userNumber = parseInt($("input#numberinputfield").val());
 
+    if (isNaN(userNumber)) {
+      alert("Please enter numbers only!")
+    }
+
+    beepboop(userNumber);
+
+    $("#result").append(userNumber);
   })
+
+
 })
